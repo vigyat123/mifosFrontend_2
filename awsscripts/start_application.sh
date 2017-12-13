dfd8125d3badf44d39106b2c4ab99840587d912d
vigyat123 <32060077+vigyat123@users.noreply.github.com>
2017-12-13 15:16:01 +0530

    Update start_application.sh

--- awsscripts/start_application.sh
+++ awsscripts/start_application.sh
@@ -53,7 +53,7 @@ http {
         # lets you doname-based virtual hosting
         #charset koi8-r;
         #access_log  logs/host.access.log  main;
-        ssl_certificate      cert.pem;
+        ssl_certificate      tomcat.crt;
         ssl_certificate_key  /usr/share/tomcat.keystore;
         ssl_session_cache    shared:SSL:1m;
         ssl_session_timeout  5m;
